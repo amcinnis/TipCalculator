3028fdef958ac745096ac96a07565870d32dbf8f
Austin McInnis <austin.mcinnis@icloud.com>
2017-05-08 16:33:13 -0700

    Finished working example for Intuit iOS Workshop. May or may not add more functionality.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var billField: UITextField!
+    @IBOutlet var tipSelect: UISegmentedControl!
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    private let tipPercentages = [0.15, 0.2, 0.25]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let bill = Double(billField.text!) ?? 0.00
+        let tip = bill * tipPercentages[tipSelect.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
